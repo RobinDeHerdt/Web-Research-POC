c8a5ed5217863bb14d3bf1d505d4d688b2887e9b
RobinDeHerdt <rdh_robin@hotmail.com>
2016-12-23 18:58:02 +0100

    Wifi tests toegevoegd

---
+++ Arduino/wifi_getfirmwareversion/wifi_getfirmwareversion.ino
@@ -0,0 +1,23 @@
+#include <SPI.h>
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600); 
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present"); 
+    // don't continue:
+    while(true);
+  } 
+
+  Serial.print("Firmware version: ");
+  Serial.println(WiFi.firmwareVersion());
+}
+
+void loop() {}
