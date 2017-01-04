dd36f28b6f5c74d5bc4fa88a043d709cfba3cbd0
RobinDeHerdt <rdh_robin@hotmail.com>
2017-01-04 15:54:57 +0100

    Data naar arduino sturen

--- Arduino/server/index.php
+++ Arduino/server/index.php
@@ -1,11 +1,50 @@
 <?php 
-	$array = [
-		0 => 1,
-		1 => 3,
-		2 => 4,
-		3 => 2
-	];
+	$conn = new mysqli('localhost', 'root', 'root', 'web_research');
+	
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+	else
+	{
+		//echo 'connected!<br>';
+	}
 
-	$jsonObject = json_encode($array);
-	echo $jsonObject;
+	$createQuery = "CREATE TABLE commands(id unsigned auto_increment primary_key, command_number int);";
+	$deleteQuery = "TRUNCATE TABLE commands";
+	$insertQuery = "INSERT INTO commands(command_number) VALUES(3),(1),(2),(4)";
+
+	if($conn->query($deleteQuery) === true)
+	{
+		//echo 'Old records deleted.<br>';
+	}
+	else
+	{
+		echo 'Error' . $deleteQuery . '<br>' . $conn->error . '<br>';
+	}
+
+	if($conn->query($insertQuery) === true)
+	{
+		//echo 'New records created.<br>';
+	}
+	else
+	{
+		echo 'Error' . $insertQuery . '<br>' . $conn->error . '<br>';
+	}
+
+	$sql = "SELECT * FROM commands";
+
+	$resultsArray = [];
+
+	if($result = $conn->query($sql))
+	{
+		foreach ($result as $key => $value) {
+			array_push($resultsArray, $value['command_number']); 
+		}
+		$result->close();
+	}
+
+	$conn->close();
+
+	$json = json_encode($resultsArray);
+	echo $json;
 ?>
